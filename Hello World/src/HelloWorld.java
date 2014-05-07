19426e7c1cc79e62eadd82d4e9a28c511d076689
catal_000 <catal_000@192.168.1.111>
2014-05-07 13:59:13 -0700

    Hello World Project

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,19 @@
+
+public class HelloWorld {
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub		
+
+      
+      String jorge = "Hello World";
+              
+      System.out.println(jorge); 
+
+   }
+
+}
+
+
+		
+
+	
